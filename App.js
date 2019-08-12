d2d791b7b3fa913cc9439aeaef5f219669d06db5
Tiago Behenck dos Santos <tiago.behenck.santos@gmail.com>
2019-08-12 15:03:52 -0300

    First test

--- App.js
+++ App.js
@@ -57,6 +57,6 @@ function handleFinishLoading(setLoadingComplete) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FAFAFA',
   },
 });
