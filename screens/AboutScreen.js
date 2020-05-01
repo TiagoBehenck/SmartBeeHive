560a8255122f6b6aecbd201ab8f279de8ee403d2
TiagoBehenck <tiago.behenck.santos@gmail.com>
2020-05-01 20:56:58 -0300

    :gear: Add new dependencies

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 
 import Colors from "../constants/Colors";
 import api from "../services/api";
@@ -8,81 +8,71 @@ export default class AboutScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      colmeias: []
+      colmeias: [],
     };
   }
 
   async componentDidMount() {
-    const response = await api.get('/conexao.php?dados={"tipo":13,"apicultor":4}')
-    
-    this.setState({ colmeias: response.data.colmeias  });
+    const response = await api.get(
+      '/conexao.php?dados={"tipo":13,"apicultor":4}'
+    );
+
+    this.setState({ colmeias: response.data.colmeias });
 
     // console.log(this.state.colmeias);
-  
   }
 
   render() {
-    
     return this.state.colmeias.length ? (
       <>
-        {this.state.colmeias.length && this.state.colmeias.map(colmeia => (
-          <View style={styles.container} key={colmeia.id}> 
+        {this.state.colmeias.length &&
+          this.state.colmeias.map((colmeia) => (
+            <View style={styles.container} key={colmeia.id}>
+              <View style={styles.label}>
+                <Text style={styles.text}>Proprietário:</Text>
+                <Text style={styles.text}>{colmeia.nome}</Text>
+              </View>
 
-            <View style={styles.label}>
-              <Text style={styles.text}>
-                Proprietário:
-              </Text>
-              <Text style={styles.text}>
-                {colmeia.nome}
-              </Text>
-            </View> 
-
-            <View  style={styles.label}>
-              <Text  style={styles.text}>
-                Descrição:
-              </Text>
-              <Text style={styles.text}>
-                {colmeia.descricao}
-              </Text>
+              <View style={styles.label}>
+                <Text style={styles.text}>Descrição:</Text>
+                <Text style={styles.text}>{colmeia.descricao}</Text>
+              </View>
             </View>
-
-          </View>
           ))}
       </>
     ) : (
       <View style={styles.container}>
-         <Text>Carregando...</Text>
-       </View>
-    )
+        <Text>Carregando...</Text>
+      </View>
+    );
   }
 }
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 30
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 30,
   },
   label: {
-    flexDirection: 'row',
+    flexDirection: "row",
     height: 60,
-    alignSelf: 'stretch',
+    alignSelf: "stretch",
     backgroundColor: `${Colors.primaryColor}`,
     borderRadius: 50,
     marginTop: 10,
-    justifyContent: 'space-around',
-    alignItems: 'center',
+    justifyContent: "space-around",
+    alignItems: "center",
   },
   text: {
     color: `${Colors.secundaryColor}`,
     fontSize: 16,
-  }
+  },
 });
 
 AboutScreen.navigationOptions = {
   title: "Sobre",
-  headerTintColor: "#F3C622"
+  headerTintColor: "#F3C622",
   // headerStyle: { backgroundColor: '#3A3637'},
 };
--- screens/DataScreen.js
+++ screens/DataScreen.js
@@ -1,67 +1,97 @@
-import React, { Component } from "react"
-import { View, StyleSheet, Text, TouchableOpacity, Platform } from "react-native";
+import React, { Component } from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Platform,
+} from "react-native";
 
-import { parseISO, format } from 'date-fns'
-import { pt } from 'date-fns/locale'
+import { parseISO, format } from "date-fns";
+import { pt } from "date-fns/locale";
 
-import TabBarIcon from '../components/TabBarIcon';
+import TabBarIcon from "../components/TabBarIcon";
 
 import Colors from "../constants/Colors";
 import api from "../services/api";
 
-// TODO Alinhar ícones a esquerda de cada botão
-// TODO Colocar a data e hora como uma "nota de rodapé" no botão 
 export default class DataScreen extends Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      leituras: []
+      leituras: [],
     };
   }
 
   async componentDidMount() {
+    const response = await api.get("/conexao.php?dados=%7B%22tipo%22:14%7D");
 
-    const response = await api.get('/conexao.php?dados=%7B%22tipo%22:14%7D')
-    
-    this.setState({ leituras: response.data.leituras  });
- 
+    this.setState({ leituras: response.data.leituras });
   }
 
   render() {
     return this.state.leituras.length ? (
       <View style={styles.container}>
-        {this.state.leituras.length && this.state.leituras.map(leitura => (
-            <TouchableOpacity key={leitura.id} onPress={() => {this.props.navigation.navigate('Log', { title: 'Histórico', id: `${leitura.id}`, um: `${leitura.um}`, title: `${leitura.sensor}` }) }} style={styles.button}>
+        {this.state.leituras.length &&
+          this.state.leituras.map((leitura) => (
+            <TouchableOpacity
+              key={leitura.id}
+              onPress={() => {
+                this.props.navigation.navigate("Log", {
+                  title: "Histórico",
+                  id: `${leitura.id}`,
+                  um: `${leitura.um}`,
+                  title: `${leitura.sensor}`,
+                });
+              }}
+              style={styles.button}
+            >
               <View style={styles.info}>
-                <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                <View style={{ flexDirection: "row", alignItems: "center" }}>
                   <TabBarIcon
-                      style={styles.buttonIcon}
-                      name={Platform.OS === 'ios' ? `${leitura.iconios}` : `${leitura.iconand}`} />
-                  <Text style={{color: `${leitura.cor}`}}> {leitura.sensor}: </Text>
+                    style={styles.buttonIcon}
+                    name={
+                      Platform.OS === "ios"
+                        ? `${leitura.iconios}`
+                        : `${leitura.iconand}`
+                    }
+                  />
+                  <Text style={{ color: `${leitura.cor}` }}>
+                    {" "}
+                    {leitura.sensor}:{" "}
+                  </Text>
                 </View>
-                  <Text style={{color: `${leitura.cor}`}}> {leitura.valor_sensor} {leitura.um}</Text>
+                <Text style={{ color: `${leitura.cor}` }}>
+                  {" "}
+                  {leitura.valor_sensor} {leitura.um}
+                </Text>
               </View>
               <View style={styles.date}>
-                <Text style={{color: '#999'}}>{format(parseISO(leitura.dataHora), "dd 'de' MMMM', às ' HH:mm'h'", { locale: pt })}</Text>
+                <Text style={{ color: "#999" }}>
+                  {format(
+                    parseISO(leitura.dataHora),
+                    "dd 'de' MMMM', às ' HH:mm'h'",
+                    { locale: pt }
+                  )}
+                </Text>
               </View>
-           </TouchableOpacity> 
+            </TouchableOpacity>
           ))}
       </View>
     ) : (
       <View style={styles.container}>
-         <Text>Carregando...</Text>
+        <Text>Carregando...</Text>
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 10
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
   },
 
   button: {
@@ -69,33 +99,32 @@ const styles = StyleSheet.create({
     backgroundColor: `${Colors.primaryColor}`,
     borderRadius: 10,
     marginTop: 10,
-    alignSelf: 'stretch',
-    justifyContent: 'center',
+    alignSelf: "stretch",
+    justifyContent: "center",
   },
 
   buttonIcon: {
-    padding: 2
+    padding: 2,
   },
 
   info: {
     marginTop: 20,
     paddingHorizontal: 50,
-    alignItems: 'center',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    padding: 5
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 5,
   },
 
   date: {
     paddingBottom: 10,
-    alignItems: 'flex-end',
-    justifyContent: 'flex-end',
-    marginRight: 50
-  }
-
+    alignItems: "flex-end",
+    justifyContent: "flex-end",
+    marginRight: 50,
+  },
 });
 
 DataScreen.navigationOptions = {
-  title: 'Sensores',
-  headerTintColor: '#F3C622',
+  title: "Sensores",
+  headerTintColor: "#F3C622",
 };
